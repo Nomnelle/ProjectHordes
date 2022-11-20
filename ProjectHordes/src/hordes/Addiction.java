29cc6fd9c723ccacc5d9e87032cbd200e0b08b8e
Del-0 <elodie.constant@etu.univ-lorraine.fr>
2022-11-20 12:09:26 +0100

    Début création attributs joueurs + getter/setter + programmations futures  fonctions Début création classe addiction + attributs addictions + getter/setter + future fonction addiction

---
+++ ProjectHordes/src/hordes/Addiction.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hordes;
+
+/**
+ *
+ * @author const
+ */
+public class Addiction {
+
+    private int CompteurDeTour;
+    private boolean TestAddiction;
+
+    public Addiction() {
+        this.CompteurDeTour = 3;
+        this.TestAddiction = false;
+    }                                  // Initialisation du compteur de tour avant perte de PV d'addiction; Joueur non addict tant que pas de boisson prise
+
+    //Getter                           // Pas forcément utiles, right ?
+    public int getCompteurDeTour() {
+        return this.CompteurDeTour;
+    }
+
+    //Setter
+    public void setCompteurDeTour(int i) {
+        this.CompteurDeTour = i;
+    }
+
+    //public void Addiction(){        // Fonction addiction
+    // Compteur de 3 à 0, décrémenté si booléen v ou f (à def), si arrive à 0, perte de PV
+    //}
+}
+
+// Addiction : si pas de prise de boisson sous 3 tours après la première, perte de 5 PV
--- ProjectHordes/src/hordes/Joueur.java
+++ ProjectHordes/src/hordes/Joueur.java
@@ -9,5 +9,70 @@ package hordes;
  * @author nomnelle
  */
 public class Joueur {
-    // Hola
+
+    public String nomJoueur;
+    private int numJoueur;                        // Pour systeme de score dans Classe Carte
+    private String[] SacADos;
+    private int pa;                               // Point d'actions, 6 au début, max 10, récupère 4 chaque tours
+    private int pv;                               // Points de vie, de 0 à 100
+    private String position[][];
+
+    public Joueur() {                              // A vérifier, éléments de retour ? Qu'est ce que je manipule exactement xD ?
+
+        // this.numJoueur =                       // Comment lier à classe Carte ?
+        this.SacADos = new String[10];
+        this.pv = 100;
+        this.pa = 6;
+        // this.position =                        // Position au début de la partie ?
+
+        // Comme pour la carte, préparer créations joueur ici via appel de fonction création ?
+    }
+
+    //Getter
+    public int getnumJoueur() {
+        return this.numJoueur;
+    }
+
+    public String[] getSacADos() {
+        return this.SacADos;
+    }
+
+    public int getpv() {
+        return this.pv;
+    }
+
+    public int getpa() {
+        return this.pa;
+    }
+
+    public String[][] Position() {
+        return this.position;
+    }
+
+    // Setter
+    public void setnumJoueur(int i) {
+        this.numJoueur = i;
+    }
+
+    //  public void setSacADos() {                          // Je ne sais pas ce que je manipule, besoin questions
+    //      this.SacADos = ;
+    // }
+    public void setpv(int i) {
+        this.pv = i;
+    }
+
+    public void setpa(int i) {
+        this.pa = i;
+    }
+
+    //  public void setposition(String [i][j]){
+    //      this.position= String[i][j];                    // Même problème que sac à dos
+    // }
+    // public int PerteDePA(){                             // Fonctions de gain et perte de PA
+    // }
+    // public int GainDePA (){
+    //}
 }
+
+
+// Completer plus tard avec info sur l'addiction
