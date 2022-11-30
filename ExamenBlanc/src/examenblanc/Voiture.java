a44d55245d38c43ab260ca9ebbca86ea137539d1
linazaarate <linazaarate@localhost>
2022-11-30 15:50:24 +0100

    test lina

---
+++ ExamenBlanc/src/examenblanc/Voiture.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenblanc;
+
+/**
+ *
+ * @author linazaarate
+ */
+public class Voiture {
+    
+    private String marque;
+    private String modele;
+    private int dateAchat;
+    private int prixAchat;
+    private int kilometrage;
+    private int prixCourant;
+    private int numVoiture;
+    
+    public void dateAchat(int year,int month, int date){
+        year=2007;
+        month=3;
+        date=15;
+    }
+    
+
+    
+    public Voiture (int n){
+        this.marque ="Renault";
+        this.modele = "Clio";
+        this.dateAchat = 2007;
+        this.prixAchat = 10000;
+        this.kilometrage = 120000;
+        this.prixCourant = 7000;
+        this.numVoiture = n;
+    }
+    
+    public static void main(String[] args){
+        Voiture []= new Voiture[1];
+        voiture.marque("Renault")
+    }
+    
+        for (Voiture v :voiture){
+            System.out.println(v);
+    
+    System.out.println(marque+modele)
+            
+    public int getPrixAchat(){
+        return this.dateAchat;
+        
+    public void prixCourant(){
+        prixCourant=prixAchat*0.05;
+    }
+    }
+    
+    
+}
