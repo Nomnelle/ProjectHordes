ad9385ed2a0d7d8f7fc7050f7885535c7c7a80d3
Del-0 <elodie.constant@etu.univ-lorraine.fr>
2022-12-03 16:53:02 +0100

    Début rédaction dans main de l'addiction

--- ProjectHordes/src/hordes/Main.java
+++ ProjectHordes/src/hordes/Main.java
@@ -14,48 +14,60 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        System.out.println(" .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.\n" +
-"| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |\n" +
-"| |  ____  ____  | || |     ____     | || |  _______     | || |  ________    | || |  _________   | || |    _______   | |\n" +
-"| | |_   ||   _| | || |   .'    `.   | || | |_   __ \\    | || | |_   ___ `.  | || | |_   ___  |  | || |   /  ___  |  | |\n" +
-"| |   | |__| |   | || |  /  .--.  \\  | || |   | |__) |   | || |   | |   `. \\ | || |   | |_  \\_|  | || |  |  (__ \\_|  | |\n" +
-"| |   |  __  |   | || |  | |    | |  | || |   |  __ /    | || |   | |    | | | || |   |  _|  _   | || |   '.___`-.   | |\n" +
-"| |  _| |  | |_  | || |  \\  `--'  /  | || |  _| |  \\ \\_  | || |  _| |___.' / | || |  _| |___/ |  | || |  |`\\____) |  | |\n" +
-"| | |____||____| | || |   `.____.'   | || | |____| |___| | || | |________.'  | || | |_________|  | || |  |_______.'  | |\n" +
-"| |              | || |              | || |              | || |              | || |              | || |              | |\n" +
-"| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |\n" +
-" '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'" );
+        System.out.println(" .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.\n"
+                + "| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |\n"
+                + "| |  ____  ____  | || |     ____     | || |  _______     | || |  ________    | || |  _________   | || |    _______   | |\n"
+                + "| | |_   ||   _| | || |   .'    `.   | || | |_   __ \\    | || | |_   ___ `.  | || | |_   ___  |  | || |   /  ___  |  | |\n"
+                + "| |   | |__| |   | || |  /  .--.  \\  | || |   | |__) |   | || |   | |   `. \\ | || |   | |_  \\_|  | || |  |  (__ \\_|  | |\n"
+                + "| |   |  __  |   | || |  | |    | |  | || |   |  __ /    | || |   | |    | | | || |   |  _|  _   | || |   '.___`-.   | |\n"
+                + "| |  _| |  | |_  | || |  \\  `--'  /  | || |  _| |  \\ \\_  | || |  _| |___.' / | || |  _| |___/ |  | || |  |`\\____) |  | |\n"
+                + "| | |____||____| | || |   `.____.'   | || | |____| |___| | || | |________.'  | || | |_________|  | || |  |_______.'  | |\n"
+                + "| |              | || |              | || |              | || |              | || |              | || |              | |\n"
+                + "| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |\n"
+                + " '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'");
         int nbJoueurs = 0;
-        while(nbJoueurs < 2 || nbJoueurs>20){
+        while (nbJoueurs < 2 || nbJoueurs > 20) {
             System.out.println("Veuillez entrer le nombre de joueurs :");
             nbJoueurs = sc.nextInt();
-            if(nbJoueurs > 20){
+            if (nbJoueurs > 20) {
                 System.out.println("20 joueurs maximum. Rentrez une autre valeur.");
-            }else if(nbJoueurs<2){
+            } else if (nbJoueurs < 2) {
                 System.out.println("Il faut au moins 2 joueurs. Rentrez une autre valeur.");
             }
         }
-                
+
         jeu(nbJoueurs);
-        
+
     }
-    
-    public static void jeu(int nbJoueurs){
-        
+
+    public static void jeu(int nbJoueurs) {
+
         Carte carte = new Carte();
         ArrayList<Joueur> listeJoueur = new ArrayList();
-        
-        for(int i=0;i<nbJoueurs;i++){
+
+        for (int i = 0; i < nbJoueurs; i++) {
             Scanner sc = new Scanner(System.in);
             String strId = String.format("%d", i);
-            System.out.println("Joueur "+strId+", rentrez votre nom :");
+            System.out.println("Joueur " + strId + ", rentrez votre nom :");
             String nom = sc.nextLine();
             Joueur j = new Joueur(i, nom);
             listeJoueur.add(j);
         }
-        
-        
+
     }
+
+    public static void Addiction(boolean TestAddiction, int Compteur) {
+        Compteur = 3;
+        while (Compteur != 0) {
+            if (TestAddiction == true) {         // Si joueur addicte, commencer à décrémenter
+                Compteur = Compteur - 1;
+            }
+        }
+        while (TestAddiction == false) {         // Si arrivé à 0, perte de PV jusque prise de boisson; Comment éviter boucle infinie ? Risque ici non ?
+            // rédiger perte de PV help
+        }
+    }
+    // Réccupérer objet addiction; true = commencer décompte + perte de -5 PV si 0 au décompte 
 }
